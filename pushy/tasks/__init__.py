b2fbd600290dc983e896b29b2ea23cde8272727f
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2014-10-12 01:02:35 +0300

    Adding device queryset filters. Fix #2

--- pushy/tasks/__init__.py
+++ pushy/tasks/__init__.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from celery import task, group
 
-from ..models import PushNotification, Device
+from ..models import PushNotification
 from ..dispatchers import get_dispatcher, Dispatcher
+from pushy.models import get_filtered_devices_queryset
 
 
 @task(queue=getattr(settings, 'PUSHY_QUEUE_DEFAULT_NAME', 'default'))
@@ -20,7 +21,12 @@ def check_pending_push_notifications():
 
 @task(queue=getattr(settings, 'PUSHY_QUEUE_DEFAULT_NAME', 'default'))
 def create_push_notification_groups(notification_id):
-    devices = Device.objects.all()
+    try:
+        notification = PushNotification.objects.get(pk=notification_id)
+    except PushNotification.DoesNotExist:
+        return False
+
+    devices = get_filtered_devices_queryset(notification)
 
     if devices.count() > 0:
         count = devices.count()
@@ -37,23 +43,25 @@ def send_push_notification_group(notification_id, offset=0, limit=1000):
     except PushNotification.DoesNotExist:
         return False
 
-    data = {
-        'title': notification.title,
-        'body': notification.body
-    }
+    devices = get_filtered_devices_queryset(notification)
 
-    keys = Device.objects.all()[offset:offset+limit]
+    devices = devices[offset:offset+limit]
 
-    for key in keys:
-        dispatcher = get_dispatcher(key.type)
-
-        result, canonical_id = dispatcher.send(key.key, data)
-
-        if result == Dispatcher.PUSH_RESULT_SENT:
-            if canonical_id > 0:
-                key.key = canonical_id
-                key.save()
-        elif result == Dispatcher.PUSH_RESULT_NOT_REGISTERED:
-            key.delete()
+    for device in devices:
+        send_single_push_notification(device, notification.get_payload())
 
     return True
+
+
+@task(queue=getattr(settings, 'PUSHY_QUEUE_DEFAULT_NAME', 'default'))
+def send_single_push_notification(device, payload):
+    dispatcher = get_dispatcher(device.type)
+
+    result, canonical_id = dispatcher.send(device.key, payload)
+
+    if result == Dispatcher.PUSH_RESULT_SENT:
+        if canonical_id > 0:
+            device.key = canonical_id
+            device.save()
+    elif result == Dispatcher.PUSH_RESULT_NOT_REGISTERED:
+        device.delete()
