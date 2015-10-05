717444123a568752c97746bbca457d6573f66985
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2015-10-05 22:03:01 +0200

    Enforce JSON format for body on admin... not very convenient for admin so a better implementation is needed here. Fixes #17

--- pushy/admin.py
+++ pushy/admin.py
@@ -1,8 +1,25 @@
+import json
 from django.contrib import admin
+from django import forms
 from models import PushNotification, Device
 
 
+class PushNotificationForm(forms.ModelForm):
+    def clean(self):
+        body = self.cleaned_data.get('body')
+        try:
+            body = json.loads(body)
+        except ValueError:
+            raise forms.ValidationError('Body does not contain valid JSON')
+        return self.cleaned_data
+
+    class Meta:
+        model = PushNotification
+        fields = ('title', 'body', 'active', 'sent', 'filter_type', 'filter_user')
+
+
 class PushNotificationAdmin(admin.ModelAdmin):
+    form = PushNotificationForm
     list_display = ('title', 'date_created', 'active', 'sent')
     list_filter = ('active', 'sent')
     search_fields = ('title', )
