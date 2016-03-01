7ae09590d5047f69a1460af0f96c8f9d53d9b5ca
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2016-03-01 13:04:45 +0100

    V 0.1.11, fixes mysql migration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Django-Pushy',
-    version='0.1.10',
+    version='0.1.11',
     author='Rakan Alhneiti',
     author_email='rakan.alhneiti@gmail.com',
 
