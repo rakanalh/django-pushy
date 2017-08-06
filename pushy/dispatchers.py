ba896c521a960e20175409be67d4527393be13cd
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2017-08-06 21:20:19 +0200

    Update APNS payload structure

--- pushy/dispatchers.py
+++ pushy/dispatchers.py
@@ -79,14 +79,13 @@ class APNSDispatcher(Dispatcher):
         try:
             response = self._client.send(
                 [token],
-                message={
-                    'alert': payload.pop('alert', None),
-                    'sound': payload.pop('sound', None),
-                    'badge': payload.pop('badge', None),
-                    'category': payload.pop('category', None),
-                    'content_available': True,
-                    'extra': payload or {}
-                }
+                title=payload.pop('title', None),
+                message=payload.pop('message', None),
+                sound=payload.pop('sound', None),
+                badge=payload.pop('badge', None),
+                category=payload.pop('category', None),
+                content_available=True,
+                extra=payload or {}
             )
 
             if response.errors:
